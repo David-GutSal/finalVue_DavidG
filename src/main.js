3b6675ca0b7fa53d24bbd689bca9638cf2a0829c
david-gutsal <davidgutierrezsalvat@gmail.com>
2026-03-09 10:53:13 +0100

    feat: estructura inicial del proyecto

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store/index'
+import axiosInstance from './plugins/axios'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+const app = createApp(App)
+
+app.config.globalProperties.$axios = axiosInstance
+
+app.use(router)
+app.use(store)
+
+app.mount('#app')
